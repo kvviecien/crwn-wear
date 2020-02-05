8c83bafa38f2f4671bcd1a31e5b1486400665a4f
Olimpia Kwiecień <kwiecien@zoho.com>
2020-02-05 23:26:04 +0100

    Moved the homepage to it's own directory. Changed the component so it's showing only the homepage.

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+import ContainerMenu from '../../components/container-menu/container-menu';
+
+const Homepage = () => (
+    <div className="homepage">
+        <ContainerMenu />
+    </div>
+);
+
+export default Homepage;
