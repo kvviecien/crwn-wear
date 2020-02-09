cc5cd1f6e8f95066a54b3522eec46cdc9f311688
Olimpia Kwiecień <kwiecien@zoho.com>
2020-02-09 22:50:58 +0100

    Update menu-items to navigate withRouter HOC

--- src/components/container-menu/container-menu.jsx
+++ src/components/container-menu/container-menu.jsx
@@ -14,33 +14,38 @@ class ContainerMenu extends React.Component {
                     title: 'hats',
                     imageUrl:
                         'https://images.unsplash.com/photo-1560774358-d727658f457c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-                    id: 1
+                    id: 1,
+                    linkUrl: 'hats'
                 },
                 {
                     title: 'jackets',
                     imageUrl:
                         'https://images.unsplash.com/photo-1544022613-e87ca75a784a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80',
-                    id: 2
+                    id: 2,
+                    linkUrl: 'jackets'
                 },
                 {
                     title: 'sneakers',
                     imageUrl:
                         'https://images.unsplash.com/photo-1521093470119-a3acdc43374a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80',
-                    id: 3
+                    id: 3,
+                    linkUrl: 'sneakers'
                 },
                 {
                     title: 'womens',
                     imageUrl:
                         'https://images.unsplash.com/photo-1508350552147-213c11fcede6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80',
                     size: 'large',
-                    id: 4
+                    id: 4,
+                    linkUrl: 'womens'
                 },
                 {
                     title: 'mens',
                     imageUrl:
                         'https://images.unsplash.com/photo-1519631128182-433895475ffe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
                     size: 'large',
-                    id: 5
+                    id: 5,
+                    linkUrl: 'mens'
                 }
             ]
         };
@@ -49,13 +54,8 @@ class ContainerMenu extends React.Component {
     render() {
         return (
             <div className="container-menu">
-                {this.state.sections.map(({ title, imageUrl, id, size }) => (
-                    <MenuItem
-                        key={id}
-                        title={title}
-                        imageUrl={imageUrl}
-                        size={size}
-                    />
+                {this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))}
             </div>
         );
