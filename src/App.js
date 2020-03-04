a9e6f62e03f30a9193438e196f98e1d8269e8b74
Olimpia Kwiecień <kwiecien@zoho.com>
2020-03-04 22:33:09 +0100

    Create Sign In and Sign Up page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Homepage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up';
 
 const HatsPage = () => (
     <div>
@@ -20,6 +21,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={Homepage} />
                 <Route path="/shop" component={ShopPage} />
+                <Route path="/signin" component={SignInSignUpPage} />
             </Switch>
         </div>
     );
