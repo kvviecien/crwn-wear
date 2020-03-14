f2a16a5a1c504b2122f6126bb6a00ec1e440cba3
Olimpia Kwiecień <kwiecien@zoho.com>
2020-03-14 18:26:52 +0100

    Implemented Sign Up with email and password
    Included ability to store authenticated users in the firestore database

--- src/pages/sign-in-sign-up/sign-in-sign-up.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sign-up';
 
 import './sign-in-sign-up.styles.scss';
 
 const SignInSignUpPage = () => (
     <div className="sign-in-sign-up">
         <SignIn />
+        <SignUp />
     </div>
 );
 
